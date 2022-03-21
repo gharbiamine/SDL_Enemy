73d4a011d759afd3c596190c6ff15c0ad2110b30
gharbiamine <gharbiamine5674@gmail.com>
2022-03-21 10:35:56 +0100

    tweaks and updates

--- enemy.c
+++ enemy.c
@@ -12,13 +12,10 @@ SDL_Rect initSDL_Rect(int x, int y, int w, int h)
 
 void initEnemy(Enemy *e)
 {
-    e->spriteSheet = IMG_Load("assets/images/image.jpg");
-    e->frame = 0;
+    e->spriteSheet = IMG_Load("./assets/images/SpriteSheet.png");
     e->direction = 0; // 0 facing right | 1 facing left
     e->posScreen = initSDL_Rect(1000, 1000, 0, 0);
-    e->posSprite = initSDL_Rect(0, 0, 100, 100);
-    e->posmax.x = 3000;
-    e->posmin.x = 2000;
+    e->posSprite = initSDL_Rect(0, 0, 108, 100);
 }
 
 void afficherEnemy(Enemy *e, SDL_Surface *screen)
@@ -31,19 +28,19 @@ void afficherEnemy(Enemy *e, SDL_Surface *screen)
 
 void animerEnemy(Enemy *e)
 {
-    e->posSprite.y = e->direction * 100;
+    e->posSprite.y = e->direction * 108;
     // loop back if the sprite sheet edge is reached
     e->posSprite.x = (e->posSprite.x == e->spriteSheet->w - e->posSprite.w) ? 0 : e->posSprite.x + 100;
 }
 
-void move(Enemy *e, int dist)
+void move(Enemy *e)
 {
     // Should turn if movement will overflow
-    if (e->posScreen.x + dist > e->posmax.x || e->posScreen.x - dist < e->posmin.x)
+    if (e->posScreen.x + 10 > POSMAX || e->posScreen.x - 10 < POSMIN)
     {
         e->direction = 1 - e->direction;
     }
-    e->posScreen.x = (e->direction == 0) ? e->posScreen.x + dist : e->posScreen.x - dist;
+    e->posScreen.x = (e->direction == 0) ? e->posScreen.x + 10 : e->posScreen.x - 10;
 }
 
 int collisionBB(SDL_Rect posp, SDL_Rect pos)
@@ -58,14 +55,21 @@ int collisionBB(SDL_Rect posp, SDL_Rect pos)
 
 void moveIA(Enemy *e, SDL_Rect posPlayer)
 {
-    if (((e->posScreen.x - posPlayer.x) < 200) && ((e->posScreen.x - posPlayer.x) > 80))
+    // player in aggro range and to the left of the enemy
+    if (((e->posScreen.x - posPlayer.x) < 200) && ((e->posScreen.x - posPlayer.x) > 100))
     {
         e->direction = 1;
         e->posScreen.x -= 10;
     }
-    else if (((e->posScreen.x - posPlayer.x) < -80) && ((e->posScreen.x - posPlayer.x) > -400))
+    // player in aggro range and to the right of the enemy
+    else if (((e->posScreen.x - posPlayer.x) > -200) && ((e->posScreen.x - posPlayer.x) < -100))
     {
         e->direction = 0;
         e->posScreen.x += 10;
+    }
+    // player out of aggro range
+    else
+    {
+        move(e);
     }
 }
--- enemy.h
+++ enemy.h
@@ -1,6 +1,8 @@
 
 #ifndef ENEMY_HEADER
 #define ENEMY_HEADER
+#define POSMIN 2000
+#define POSMAX 3000
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,17 +12,16 @@
 typedef struct Enemy
 {
     SDL_Surface *spriteSheet;
+    // Sprite Sheet is 400*216
     SDL_Rect posScreen, posSprite;
     int direction;
-    SDL_Rect posmin, posmax;
-    int frame;
 } Enemy;
 
 SDL_Rect initSDL_Rect(int x, int y, int w, int h);
 void initEnemy(Enemy *e);
 void afficherEnemy(Enemy *e, SDL_Surface *screen);
 void animerEnemy(Enemy *e);
-void move(Enemy *e, int dist);
+void move(Enemy *e);
 int collisionBB(SDL_Rect posp, SDL_Rect pos);
 void moveIA(Enemy *e, SDL_Rect posPlayer);
 
